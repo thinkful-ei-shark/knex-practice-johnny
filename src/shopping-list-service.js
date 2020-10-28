4e3ece3b97144f457e606550eacbe151f0ad2b3b
johnnyjuarez <johnnyjuarez1992@gmail.com>
2020-10-28 15:02:18 -0500

    added data test and insert tests

--- src/shopping-list-service.js
+++ src/shopping-list-service.js
@@ -1,6 +1,13 @@
 const shoppingListService = {
   getAllLists(knex) {
-    return knex.select().from('shopping_list');
+    return knex.select('*').from('shopping_list');
+  },
+  insertListItem(knex, newItem) {
+    return knex
+      .insert(newItem)
+      .into('shopping_list')
+      .returning('*')
+      .then(rows => rows[0]);
   }
 };
 
--- test/shopping-list-services.spec.js
+++ test/shopping-list-services.spec.js
@@ -15,35 +15,88 @@ describe('Shopping List service object', () => {
     {
       id: 1,
       name: 'test 1',
-      price: 20.20,
+      price: '20.20',
       date_added: new Date(),
-      checked: false,
-      category: 'snack'
+      category: 'Snack'
     },
     {
       id: 2,
       name: 'test 2',
-      price: 20.30,
+      price: '20.30',
       date_added: new Date(),
-      checked: false,
-      category: 'snack'
+      category: 'Snack'
     },
     {
       id: 3,
       name: 'test 3',
-      price: 20.44,
+      price: '20.44',
       date_added: new Date(),
-      checked: false,
-      category: 'snack'
+      category: 'Snack'
     }
   ];
   // ? before testing set db to be our knex config
   before(() => {
     db = knex({
       client: 'pg',
-      connection: process.env.DB_URL
+      connection: process.env.TEST_DB_URL,
     });
   });
 
+  // ? truncate db so that we have a clean db to work with
+  before(() => db('shopping_list').truncate());
 
+  // ? truncate after each test
+  afterEach(() => db('shopping_list').truncate());
+
+  // ? after all tests destroy connection to database
+  after(() => db.destroy());
+
+  // ? test for blogful_articles having data
+  context('Given "shopping_list" has data', () => {
+    beforeEach(() => {
+      return db
+        .into('shopping_list')
+        .insert(testLists);
+    });
+    it('getAllItems() resolves all items from "shopping_list" table', () => {
+    // test that shoppinngListServices
+      const expectedItems = testLists.map(list => ({
+        ...list,
+        checked: false
+      }));
+      return shoppingListService.getAllLists(db)
+        .then(actual => {
+          expect(actual).to.eql(expectedItems);
+        })
+    })
+
+  });
+  context('Given "shopping_list" does not have data', () => {
+    it('getAllArticles() resolves an empty array', () => {
+      return shoppingListService.getAllLists(db)
+      .then(actual => {
+        expect(actual).to.eql([]);
+      });
+    })
+    it('insertListItem() inserts a new item and resolves the new item with an id', () => {
+      const newItem = {
+        name: 'Test new name',
+        price: '33.33',
+        date_added: new Date(),
+        category: 'Breakfast',
+        checked: false
+      }
+      return shoppingListService.insertListItem(db, newItem)
+        .then(actual => {
+          expect(actual).to.eql({
+            id: 1,
+            name: newItem.name,
+            price: newItem.price,
+            date_added: newItem.date_added,
+            category: newItem.category,
+            checked: newItem.checked
+          })
+        })
+    })
+  })
 });
